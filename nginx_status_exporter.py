29cbde8eb8601d704896120ac995323b3997176f
simanchou <zhouan@sensefun.com>
2017-12-12 15:07:21 +0800

    version 0.1

--- nginx_status_exporter.py
+++ nginx_status_exporter.py
@@ -8,6 +8,7 @@
 import requests
 import yaml
 import os
+import time
 
 
 def getConf(file="conf.yml"):
@@ -21,13 +22,62 @@ def getConf(file="conf.yml"):
         return None
 
 
-url = "http://192.168.50.40/nginx_status"
-c = requests.get(url)
-r = c.content.decode("utf-8").splitlines()
-print(r)
+def getNginxStatus(url):
+    statusDict = {}
+    try:
+        r = requests.get(url)
+        c = r.content.decode("utf-8").splitlines()
+        statusDict["nginx_up"] = 1
+        statusDict["connections"] = int(c[0].split(":")[-1].strip())
+        statusDict["accepts"] = int(c[2].split()[0].strip())
+        statusDict["handled"] = int(c[2].split()[1].strip())
+        statusDict["requests"] = int(c[2].split()[2].strip())
+        statusDict["reading"] = int(c[3].split()[1].strip())
+        statusDict["writing"] = int(c[3].split()[3].strip())
+        statusDict["waiting"] = int(c[3].split()[5].strip())
+        #print(statusDict)
+    except:
+        statusDict["nginx_up"] = 0
+    return statusDict
+
+
+def exportToGateway(nginxStatusUrl, gateway, job, group, instance, host, env, service):
+    gwUrl = "http://{}/metrics/job/{}/group/{}/instance/{}/host/{}/env/{}/service/{}".format(gateway,
+                                                                                             job,
+                                                                                             group,
+                                                                                             instance,
+                                                                                             host,
+                                                                                             env,
+                                                                                             service)
+    nginxStatus = getNginxStatus(nginxStatusUrl)
+    if nginxStatus["nginx_up"]:
+        data = "nginx_up{} {}\n".format("{}", 1)
+        for k, v in nginxStatus.items():
+            if k != "nginx_up":
+                data += "nginx_status{} {}\n".format("{}state=\"{}\"{}".format("{", k, "}"), nginxStatus[k])
+    else:
+        data = "nginx_up{} {}\n".format("{}", 0)
+    #print(data)
+    r = requests.put(gwUrl, data=data)
+    return r.text
 
 
 if __name__ == "__main__":
     conf = getConf()
-    print(type(conf))
-    print(conf)
+
+    gateway = conf["global"]["gateway"]
+    interval = conf["global"]["interval"]
+
+    while True:
+        for i in conf["target_configs"]:
+            job = i["job"]
+            for j in i["static_configs"]:
+                group = j["group"]
+                host = j["host"]
+                env = j["env"]
+                service = j["service"]
+                for instance in j["targets"]:
+                    nginxStatusUrl = instance
+                    instance = instance.split("/")[2]
+                    exportToGateway(nginxStatusUrl,gateway,job,group,instance,host,env,service)
+        time.sleep(interval)
